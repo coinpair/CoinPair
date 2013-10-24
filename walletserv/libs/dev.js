5e6d2cab10dfbc8432ca00eb1752b5fb978d2105
Jacob <jacob@torba.com>
2013-10-23 22:20:11 -0700

    balance on adminpage

--- walletserv/libs/dev.js
+++ walletserv/libs/dev.js
@@ -1,4 +1,9 @@
 function dev(res, database) {
+	var wallet = require('../libs/wallet.js'),
+		config = require('./../config.js'),
+		async = require('async');
+		
+	wallet = new wallet();
 	database.devbase.list(function(err, rows) {
 		if (err) res.send('Couldnt contact database');
 		else {
@@ -6,16 +11,28 @@ function dev(res, database) {
 			var response = '<html>';
 			response += '<head><title>dev page</title></head>';
 			response += '<body>';
-			response += '<h1>Listing all processed transactions</h1>';
-			response += '<ul>';
-			for (var i = rows.rowCount -1; i != -1; i--) {
-				var element = array[i];
-				response += '<li><b style="background-color: ' + color(element.hash) + '">' + element.hash + '</b> at ' + element.date + ' [' + element.conversion + '] - ' + element.type + '</li>';
-			}
-			response += '</ul>'
-			response += '</body>';
-			response += '</html>';
-			res.send(response);
+			response += '<h1>Listing Balances</h1><ul>';
+			async.forEach(config.allow.from, function(item, callback) {
+				wallet.find(item).getBalance('*', 1, function(err, balance) {
+					if (err) response += '<li>Error for ' + item + ': ' + err + '</li>';
+					else response += '<li>' + item + ': ' + balance + '</li>';
+					callback();
+				});
+			}, function(err) {
+				response += '</ul>';
+				response += '<h1>Listing all processed transactions</h1>';
+				response += '<ul>';
+				for (var i = rows.rowCount - 1; i != -1; i--) {
+					var element = array[i];
+					response += '<li><b style="background-color: ' + color(element.hash) + '">' + element.hash + '</b> at ' + element.date + ' [' + element.conversion + '] - ' + element.type + '</li>';
+				}
+				response += '</ul>'
+				response += '</body>';
+				response += '</html>';
+				res.send(response);
+			});
+
+
 		}
 	});
 }
